940fcdba6a32260c372b04bf2e1e79a0208fac14
ppajersk <ppajersk@cisco.com>
2021-02-23 02:39:23 -0500

    Routine Commit
    IntegrateRecords.py tested and works. Runs the integration and
    cleans records afterwards.
    
    Lambda-nb-53.py updated to match - not tested

--- IntegrateRecords.py
+++ IntegrateRecords.py
@@ -1,9 +1,9 @@
 import datetime
-from Netbox_Route53_integration import NetboxRoute53
+from Netbox_Route53.Netbox_route53 import NetboxRoute53
 
 if __name__ == '__main__':
     start_time = datetime.datetime.now()
-    netbox_r53 = MerakiNetbox()
+    netbox_r53 = NetboxRoute53()
     netbox_r53.integrate_records()
     netbox_r53.clean_r53_records()
     end_time = datetime.datetime.now()
--- lambda-NB-53.py
+++ lambda-NB-53.py
@@ -1,16 +1,15 @@
 import logging
 import datetime
-from Netbox_Route53_Integration.Netbox_Route53 import Netbox_route53
+from Netbox_Route53.Netbox_route53 import NetboxRoute53
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-
-#pylint: disable=unused-argument
 def lambda_handler(event, context):
     logger.debug('new event received: %s', str(event))
     start_time = datetime.datetime.now()
-    Netbox_route53 = NetboxRoute53()
-    Netbox_route53.integrate_records()
+    netbox_r53 = NetboxRoute53()
+    netbox_r53.integrate_records()
+    netbox_r53.clean_r53_records()
     end_time = datetime.datetime.now()
     logger.debug('Script complete, total runtime {%s - %s}', end_time, start_time)
