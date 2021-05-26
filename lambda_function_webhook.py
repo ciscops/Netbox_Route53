3428d8ca9f05091ee166439c145878b731bb6442
ppajersk <ppajersk@cisco.com>
2021-05-25 23:16:34 -0400

    Changed lambda function into two separate ones Adjusted makefile for this change Updated readme slighty, wip

--- lambda_function_webhook.py
+++ lambda_function_webhook.py
@@ -1,16 +1,28 @@
 import logging
 import datetime
+import json
+import socket
 from Netbox_Route53.Netbox_route53 import NetboxRoute53
 
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 
 def lambda_handler(event, handle):
     logger.debug('new event received: %s', str(event))
+    logger.debug(str(event))
+    logger.debug(socket.gethostbyname('netbox3.aws.ciscops.net'))
     start_time = datetime.datetime.now()
     netbox_r53 = NetboxRoute53()
-    netbox_r53.integrate_records()
-    netbox_r53.clean_r53_records()
+    netbox_r53.webhook_update_record(event)
     end_time = datetime.datetime.now()
     logger.debug('Script complete, total runtime {%s - %s}', end_time, start_time)
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "testkey ": "testval"
+        })
+    }
